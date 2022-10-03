57039ff2cc3a950bf8445f42a7effd76ce42bbce
Madebybrown <madebybrown@gmail.com>
2022-10-03 12:11:25 +0000

    Calculate surplus data

--- run.py
+++ run.py
@@ -74,11 +74,17 @@ def calculate_surplus_data(sales_row):
     - Positive surplus indicated waste
     - Negative surplus indicates extra made when stock was sold out.
     """
-    print('Calculation surplus data...\n')
+    print('Calculating surplus data...\n')
     stock = SHEET.worksheet('stock').get_all_values()
     stock_row = stock[-1]
-    print(stock_row)
 
+    surplus_data = []
+    for stock, sales in zip(stock_row, sales_row):
+        surplus = int(stock) - sales
+        surplus_data.append(surplus)
+
+    return surplus_data
+        
 def main():
     """
     Run all program functions
@@ -86,7 +92,8 @@ def main():
     data = get_sales_data()
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
-    calculate_surplus_data(sales_data)
+    new_surplus_data = calculate_surplus_data(sales_data)
+    print(new_surplus_data)
 
 print('Welcome to love Sandwiches Data Automation')
 main()
